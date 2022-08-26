af7a1ecd7528525feb60e8507c16fc0b63365d84
Mei Kwok <heik922@yahoo.com.hk>
2022-08-26 11:20:24 -0400

    BubbleSort, SelectionSort and MergeSort

---
+++ sortingAlgorithms.js
@@ -0,0 +1,57 @@
+// --- Directions
+// Implement bubbleSort, selectionSort, and mergeSort
+
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let min = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[min]) {
+        min = j;
+      }
+    }
+    if (arr[i] !== arr[min]) {
+      let temp = arr[min];
+      arr[min] = arr[i];
+      arr[i] = temp;
+    }
+  }
+  return arr;
+}
+
+function mergeSort(arr) {
+  if (arr.length === 1) return arr;
+
+  // [ 0 , 1 , 2, 3 ] -> [0,1] [2,3]
+  const mid = Math.floor(arr.length / 2);
+  const left = arr.slice(0, mid);
+  const right = arr.slice(mid);
+
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left, right) {
+  let result = [];
+
+  while (left.length && right.length) {
+    if (left[0] < right[0]) {
+      result.push(left.shift());
+    } else {
+      result.push(right.shift());
+    }
+  }
+
+  return [...result, ...left, ...right];
+}
